b1e27f6a30ef0464e44d0e3a94e4e82489cbe6b1
Bogdan Sataric <bsataric@cern.ch>
2020-04-03 15:52:28 +0200

    Added tasks option in SmartRenameDialog.java

--- src/confdb/gui/SmartRenameDialog.java
+++ src/confdb/gui/SmartRenameDialog.java
@@ -56,7 +56,7 @@ public class SmartRenameDialog extends JDialog
 	jTextNewPattern.setEditable(true);
 	jButtonOk.setEnabled(true);
 	jButtonCancel.setEnabled(true);
-	setTitle("Change substrings within module/sequence/path names");
+	setTitle("Change substrings within module/sequence/task/path names");
     }
     
 
@@ -145,6 +145,7 @@ public class SmartRenameDialog extends JDialog
 	m.addElement("All");
 	m.addElement("Paths");
 	m.addElement("Sequences");
+	m.addElement("Task");
 	m.addElement("Modules");
 
 	jComboBoxType.addActionListener(new ActionListener() {
