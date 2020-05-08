42d72b245594fb921d50cb7ed252cdb80284d619
Bogdan Sataric <bsataric@cern.ch>
2020-05-08 18:08:09 +0200

    Eclipse formatting done

--- src/confdb/db/DatabaseException.java
+++ src/confdb/db/DatabaseException.java
@@ -1,35 +1,30 @@
 package confdb.db;
 
 /**
- * DatabaseException
- * -----------------
+ * DatabaseException -----------------
+ * 
  * @author Philipp Schieferdecker
  *
- * Exception signature of the confdb.db. package.
+ *         Exception signature of the confdb.db. package.
  */
-public class DatabaseException extends Exception
-{
-    //
-    // construction
-    //
-    
-    /** default constructor */
-    public DatabaseException()
-    {
+public class DatabaseException extends Exception {
+	//
+	// construction
+	//
 
-    }
+	/** default constructor */
+	public DatabaseException() {
 
-    /** constructor with error message */
-    public DatabaseException(String errMsg)
-    {
-	super(errMsg);
-    }
-    
-    /** constructor with error message and nested exception */
-    public DatabaseException(String errMsg,Throwable e)
-    {
-	super(errMsg,e);
-    }
-    
+	}
+
+	/** constructor with error message */
+	public DatabaseException(String errMsg) {
+		super(errMsg);
+	}
+
+	/** constructor with error message and nested exception */
+	public DatabaseException(String errMsg, Throwable e) {
+		super(errMsg, e);
+	}
+
 }
-
